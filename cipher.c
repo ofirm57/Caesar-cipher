a84e48ae045339c806c89f0ab6f0fcdc342291df
ofir <ofirm57@gmail.com>
2025-05-28 13:00:39 +0300

    first commit

---
+++ cipher.c
@@ -0,0 +1,52 @@
+#include "cipher.h"
+#include <string.h>
+/// IN THIS FILE, IMPLEMENT EVERY FUNCTION THAT'S DECLARED IN cipher.h.
+
+
+
+// Macros
+#define BIG_MIN = 96
+#define BIG_MAX = 123
+#define SMALL_MIN = 64
+#define SMALL_MAX = 91
+#define NUM_LETTERS = 26
+
+
+
+void change_s (char s[], int k, int min_val, int max_val)
+{
+  int i = 0;
+  while (s[i] != '\0')
+  {
+    if(s[i] > min_val && s[i] < max_val)
+    {
+      if ( (s[i] + (k % NUM_OF_LETTER) > max val)
+      {
+        s[i] = s[i] - (NUM_LETTERS - k % NUM_LETTERS);
+      }
+      else
+      {
+        if( K>=0 || min_val < s[i] + (k % NUM_OF_LETTER) )
+        { s[i] = s[i] + (k % NUM_LETTERS); }
+        else
+        {
+          s[i] = s[i] + (NUM_LETTERS + (k % NUM_LETTERS) );
+        }
+      }
+    }
+    i++;
+  }
+}
+
+// See full documentation in header file
+void encode (char s[], int k)
+{
+  change_s (s, k, int BIG_MIN, int BIG_MAX); //Big letters
+  change_s (s, k, int SMALL_MIN, int SMALL_MAX); // small letters
+}
+// See full documentation in header file
+void decode (char s[], int k)
+{
+  change_s (s, -k, int BIG_MIN, int BIG_MAX); //Big letters
+  change_s (s, -k, int SMALL_MIN, int SMALL_MAX); // small letters
+}
---
+++ main.c
@@ -0,0 +1,12 @@
+#include "cipher.h"
+#include "tests.h"
+#include <string.h>
+
+
+
+// your code goes here
+
+int main (int argc, char *argv[])
+{
+  // your code goes here
+}
