d8ec40e2fa0f704335be85d6b634e06ee2c3e563
ofir <ofirm57@gmail.com>
2025-05-28 15:22:41 +0300

    write test.c

--- tests.c
+++ tests.c
@@ -1,8 +1,12 @@
+#include <string.h>
 #include "tests.h"
 
 #define K_1 3
+#define K_2 2
+#define MINUS_K -1
+#define MINUS3_K -3
+#define K_29 29
 
-// See full documentation in header file
 int test_encode_non_cyclic_lower_case_positive_k ()
 {
   char in[] = "abc";
@@ -11,31 +15,38 @@ int test_encode_non_cyclic_lower_case_positive_k ()
   return strcmp (in, out) != 0;
 }
 
-// See full documentation in header file
 int test_encode_cyclic_lower_case_special_char_positive_k ()
 {
-  // your code goes here
+  char in[] = " yY,zZ.";
+  char out[] = " aA,bB.";
+  encode (in, K_2);
+  return strcmp (in, out) != 0;
 }
 
-// See full documentation in header file
 int test_encode_non_cyclic_lower_case_special_char_negative_k ()
 {
-  // your code goes here
+  char in[] = "$bB$kK% ";
+  char out[] = "$aA$jJ% ";
+  encode (in, MINUS_K);
+  return strcmp (in, out) != 0;
 }
 
-// See full documentation in header file
 int test_encode_cyclic_lower_case_negative_k ()
 {
-  // your code goes here
+  char in[] = "rilu";
+  char out[] = "ofir";
+  encode (in, MINUS3_K);
+  return strcmp (in, out) != 0;
 }
 
-// See full documentation in header file
 int test_encode_cyclic_upper_case_positive_k ()
 {
-  // your code goes here
+  char in[] = "XYZ";
+  char out[] = "ABC";
+  encode (in, K_29);
+  return strcmp (in, out) != 0;
 }
 
-// See full documentation in header file
 int test_decode_non_cyclic_lower_case_positive_k ()
 {
   char in[] = "def";
@@ -44,26 +55,36 @@ int test_decode_non_cyclic_lower_case_positive_k ()
   return strcmp (in, out) != 0;
 }
 
-// See full documentation in header file
 int test_decode_cyclic_lower_case_special_char_positive_k ()
 {
-  // your code goes here
+  {
+    char in[] = " aA,bB.";
+    char out[] = " yY,zZ.";
+    decode (in, K_2);
+    return strcmp (in, out) != 0;
+  }
 }
 
-// See full documentation in header file
 int test_decode_non_cyclic_lower_case_special_char_negative_k ()
 {
-  // your code goes here
+  char in[] = "$aA$jJ ";
+  char out[] = "$bB$kK ";
+  decode (in, MINUS_K);
+  return strcmp (in, out) != 0;
 }
 
-// See full documentation in header file
 int test_decode_cyclic_lower_case_negative_k ()
 {
-  // your code goes here
+  char in[] = "ofir";
+  char out[] = "rilu";
+  decode (in, MINUS3_K);
+  return strcmp (in, out) != 0;
 }
 
-// See full documentation in header file
 int test_decode_cyclic_upper_case_positive_k ()
 {
-  // your code goes here
+  char in[] = "ABC";
+  char out[] = "XYZ";
+  decode (in, K_29);
+  return strcmp (in, out) != 0;
 }
